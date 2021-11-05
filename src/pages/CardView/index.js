20ff05e758e76fb3b5cc4243469a091200916d68
BrijeshMakwana18 <bamakwana2000@gmail.com>
2021-11-05 14:57:27 +0530

    card ui implemented

--- src/pages/CardView/index.js
+++ src/pages/CardView/index.js
@@ -1,10 +1,79 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useRef} from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
 import styles from './styles';
+import {colors, fonts, images, perfectSize} from '../../theme';
+const sizesData = [{label: 8}, {label: 9}, {label: 10}, {label: 11}];
+const colorsData = [{color: '#CA0A1C'}, {color: '#A6E040'}, {color: '#42B0F7'}];
 export default function CardView() {
+  //RenderItem for sizes and color selector
+  const renderItem = (item, type) => {
+    const isSizes = type == 'sizes' ? true : false;
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: perfectSize(30),
+          width: perfectSize(30),
+          backgroundColor: isSizes ? colors.primaryLightColor : item.color,
+          borderRadius: perfectSize(isSizes ? 5 : 15),
+          marginLeft: perfectSize(10),
+        }}>
+        {isSizes && <Text>{item.label}</Text>}
+      </View>
+    );
+  };
+  const renderSizesAndColors = type => {
+    const isSizes = type == 'sizes' ? true : false;
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: isSizes ? 0 : perfectSize(20),
+        }}>
+        <Text
+          style={{
+            fontFamily: fonts.quicksandBold,
+            color: colors.primaryLightColor,
+            fontSize: perfectSize(16),
+          }}>
+          {isSizes ? 'SIZES : ' : 'COLORS : '}
+        </Text>
+        <View>
+          <FlatList
+            data={isSizes ? sizesData : colorsData}
+            horizontal
+            renderItem={({item, index}) => renderItem(item, type)}
+            scrollEnabled={false}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </View>
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Animated Card UI</Text>
+      <StatusBar translucent barStyle="light-content" />
+      <TouchableOpacity
+        style={styles.cardView}
+        onPress={() => handleOnCardPress()}>
+        <Image source={images.shoe} style={styles.cardImage} />
+        <Text style={styles.cardTitle}>{'Nike Shoes'}</Text>
+        {renderSizesAndColors('sizes')}
+        {renderSizesAndColors('colors')}
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonTitle}>{'Add To Cart'}</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/pages/CardView/styles.js
+++ src/pages/CardView/styles.js
@@ -8,3 +8,32 @@ export default styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: colors.primaryBackgroundColor,
   },
+  cardView: {
+    height: '50%',
+    width: '80%',
+    backgroundColor: colors.cardBackgroundColor,
+    borderRadius: perfectSize(20),
+    alignItems: 'center',
+  },
+  cardImage: {
+    height: '75%',
+    width: '100%',
+  },
+  cardTitle: {
+    color: colors.primaryLightColor,
+    fontFamily: fonts.quicksandBold,
+    fontSize: perfectSize(30),
+  },
+  buttonContainer: {
+    height: '15%',
+    width: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: perfectSize(10),
+    backgroundColor: colors.primaryLightColor,
+  },
+  buttonTitle: {
+    fontFamily: fonts.quicksandBold,
+    fontSize: perfectSize(20),
+  },
+});
